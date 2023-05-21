e58b051e01cc2f2db9cd41ca6c7ab7b610440d2a
Yusuf Yağcı <98044490+Yusufygc@users.noreply.github.com>
2023-05-21 17:49:28 +0300

    finish

--- iletisim.php
+++ iletisim.php
@@ -1,12 +1,36 @@
-<b>Veriler alındı</b>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="styles/iletisimphp.css">
+</head>
+<body>
+  
+<div class="box">
 <?php
+$ad=$_POST["isim-soyisim"];
+$email=$_POST["e-posta"];
+$mesaj=$_POST["mesaj"];
+$alan=$_POST["alan"];
+$secimler = $_POST['secimler'];
 
-
-
-
-
-
-
+echo "<b>Adınız ve soyadınız : </b>".$ad."<br> <br>";
+echo "<b>E-posta adresiniz : </b> ".$email."<br> <br>";
+echo "<b>Öğrenmek istediğiniz alan :  </b>".$alan."<br> <br>";
+echo "<b>Mesajınız : </b> ".$mesaj."<br> <br>";
+echo "<b>Beğendiğiniz sayfalar : </b> <br> <br>";
+foreach($secimler as $secim) {
+    echo $secim . "<br>";
+   
+  }
 
 ?>
-<a href="1_girisSayfasi.html">Giriş sayfasına dön</a>
+<div class="donus"><a href="2_hakkimda.html">Hakkımda sayfasına dön</a></div>
+
+</div>
+</body>
+</html>
